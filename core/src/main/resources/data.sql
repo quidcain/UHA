59512abd2373ca6f2a7c9d02fb4478babed5b23b
quidcain <quidcain@gmail.com>
2018-03-16 13:40:52 +0300

    Add more sample data

--- core/src/main/resources/data.sql
+++ core/src/main/resources/data.sql
@@ -1,9 +1,17 @@
 insert into eptituders (name, side)
-VALUES ('Bloodyslav', 'Opposition');
+VALUES ('Bloodyslav', 'evil');
 insert into eptituders (name, side)
-VALUES ('Danuke', 'Opposition');
+VALUES ('Danuke', 'evil');
 insert into eptituders (name, side)
-VALUES ('Alethunder', 'Opposition');
+VALUES ('Alethunder', 'good');
 
 INSERT INTO clans (name, side) VALUES ('akatsuke', 'evil');
 UPDATE eptituders SET clan_id = 1 WHERE id=2;
+
+INSERT INTO events (name, description, date, status)
+VALUES ('Avenger\'s creation',
+        'Loki gains access to the Tesseract, Nick Fury creates Avengers to save Earth',
+        DATE '2012-5-1',
+        'finished');
+INSERT INTO eptituders_events
+VALUES (1, 1);
